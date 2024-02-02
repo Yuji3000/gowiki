892b1fa30825f8560b526e94bf158a15bca69610
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2024-02-01 17:35:19 -0700

    Wip adding save

--- wiki.go
+++ wiki.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"net/http"
+	"regexp"
 )
 
 type Page struct {
@@ -12,6 +13,8 @@ type Page struct {
 	Body []byte
 }
 
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
@@ -29,19 +32,14 @@ func loadPage(title string) (*Page, error) {
 
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, err := template.ParseFiles(tmpl + ".html")
-	if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-	}
-	err = t.Execute(w, p)
+	err := template.ExcecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
+	title, err := getTitle(w, r)
 	p, err := loadPage(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit"+title, http.StatusFound)
@@ -51,7 +49,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+	title, err := getTitle(w, r)
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
@@ -60,10 +58,10 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+	title, err := getTitle(w, r)
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
-	err := p.save()
+	err = p.save()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -71,6 +69,14 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+			http.NotFound(w, r)
+			return "", errors.New("invalid Page Title")
+	}
+	return m[2], nil 
+}
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
